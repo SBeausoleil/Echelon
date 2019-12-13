4559948e747669bf6f8c750995aed9bba3b6baa1
Samuel Beausoleil <samuel.beausoleil@hotmail.com>
2019-12-13 03:44:40 -0500

    Added ability to load from external properties file

--- src/main/java/com/sb/echelon/configuration/JdbcConfig.java
+++ src/main/java/com/sb/echelon/configuration/JdbcConfig.java
@@ -2,18 +2,27 @@ package com.sb.echelon.configuration;
 
 import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
+@PropertySource("classpath:echelon.properties")
 public class JdbcConfig {
 
+	@Value("${datasource.url}")
+	private String url;
+	@Value("${datasource.username}")
+	private String user;
+	@Value("${datasource.password}")
+	private String password;
+	
 	@Bean
 	public SimpleDriverDataSource simpleDriverDataSource() throws SQLException {
-		return new SimpleDriverDataSource(new com.mysql.cj.jdbc.Driver(),
-				"jdbc:mysql://localhost:3306/echelon?serverTimezone=UTC", "echelon", "echelon");
+		return new SimpleDriverDataSource(new com.mysql.cj.jdbc.Driver(), url, user, password);
 	}
 
 	@Bean
