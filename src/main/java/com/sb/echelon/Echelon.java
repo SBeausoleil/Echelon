02254c099c4dad89efb5a33f64e96118d9efd325
Samuel Beausoleil <samuel.beausoleil@hotmail.com>
2019-12-13 09:23:54 -0500

    Added some documentation

--- src/main/java/com/sb/echelon/Echelon.java
+++ src/main/java/com/sb/echelon/Echelon.java
@@ -53,7 +53,9 @@ public final class Echelon {
 	private HashSet<AnalyzedClass<?>> existingTables = new HashSet<>();
 
 	public void analyze(Class<?>... classes) throws NoIdFieldException {
-		for (Class<?> clazz : classes) { analyzedClasses.put(clazz, analyzer.analyze(clazz)); }
+		for (Class<?> clazz : classes) {
+			analyzedClasses.put(clazz, analyzer.analyze(clazz));
+		}
 	}
 
 	@SuppressWarnings("unchecked")
@@ -70,9 +72,11 @@ public final class Echelon {
 	public <T> AnalyzedClass<T> getAnalyzed(Class<T> clazz) {
 		return (AnalyzedClass<T>) analyzedClasses.get(clazz);
 	}
+
 	public <T> ColumnParser<T> getParserFor(Class<T> type) {
 		return parserRecommander.getParserFor(type);
 	}
+
 	public <T> SqlInsertionPreparer<T> getPreparerFor(Class<T> type) {
 		return preparerRecommander.getPreparerFor(type);
 	}
@@ -80,10 +84,19 @@ public final class Echelon {
 	public String getSuggestionFor(Class<?> type) {
 		return typeRecommander.getSuggestionFor(type);
 	}
+
 	public boolean hasAnalyzed(Class<?> clazz) {
 		return analyzedClasses.containsKey(clazz);
 	}
 
+	/**
+	 * Load an object of the desired class.
+	 * 
+	 * @param <T>   the type of the object
+	 * @param clazz the class of the object
+	 * @param id    the object's id
+	 * @return the object or null if none was found.
+	 */
 	@SuppressWarnings("unchecked")
 	public <T> T load(Class<T> clazz, long id) {
 		AnalyzedClass<T> analyzed = (AnalyzedClass<T>) analyzedClasses.get(clazz);
@@ -124,8 +137,6 @@ public final class Echelon {
 		return typeRecommander.putSuggestion(forType, sqlType);
 	}
 
-	
-	
 	public String putTypeSuggestionIfAbsent(Class<?> forType, String sqlType) {
 		return typeRecommander.putSuggestionIfAbsent(forType, sqlType);
 	}
@@ -136,7 +147,9 @@ public final class Echelon {
 			if (!tableWriter.tableExists(analyzed)) {
 				writeTable(analyzed);
 				existingTables.add(analyzed);
-			} else if (!existingTables.contains(analyzed)) { existingTables.add(analyzed); }
+			} else if (!existingTables.contains(analyzed)) {
+				existingTables.add(analyzed);
+			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
@@ -144,6 +157,14 @@ public final class Echelon {
 		return (AnalyzedClass<T>) analyzedClasses.put(analyzed.getTargetClass(), analyzed);
 	}
 
+	/**
+	 * Save an object in the database.
+	 * 
+	 * @param <T> the type of the object
+	 * @param obj the object to save
+	 * @return the object after the save operation. If it was attributed an ID
+	 *         during the operation, it will have been set.
+	 */
 	@SuppressWarnings("unchecked")
 	public <T> T save(T obj) {
 		AnalyzedClass<T> analyzed = (AnalyzedClass<T>) analyzedClasses.get(obj.getClass());
@@ -151,7 +172,7 @@ public final class Echelon {
 			analyzed = (AnalyzedClass<T>) analyze(obj.getClass());
 		return saveWriter.save(obj, analyzed);
 	}
-	
+
 	protected void writeTable(AnalyzedClass<?> analyzed) {
 		tableWriter.writeTable(analyzed);
 	}
